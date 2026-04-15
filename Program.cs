bc63b036ddbe407e0b6506bd43f1aa44abeb0ed6
Teolana09 <lanari.matteo@franchettisalviani.net>
2026-04-15 13:35:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EserciziFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cartella = "Configrazione";
+            string nomeFile = "impostazioni.log";
+            if (!Directory.Exists(cartella))
+            {
+                Directory.CreateDirectory(cartella);
+            }
+            string path = Path.Combine(cartella, nomeFile);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            
+        }
+    }
+}
