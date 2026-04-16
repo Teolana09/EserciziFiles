c77817a0da1f5dd679aafd6152474ddc18a11f4a
Teolana09 <lanari.matteo@franchettisalviani.net>
2026-04-16 10:47:18 +0200

    mod3

--- Program.cs
+++ Program.cs
@@ -4,18 +4,47 @@
     {
         static void Main(string[] args)
         {
-            string cartella = "Configrazione";
+            
+            DateTime ora = DateTime.Now;
+            string utente;
+            string sistema;
+            string Risoluzione;
+
+            string cartella = "Configurazione";
             string nomeFile = "impostazioni.log";
             if (!Directory.Exists(cartella))
             {
                 Directory.CreateDirectory(cartella);
             }
             string path = Path.Combine(cartella, nomeFile);
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
             
+            
+            Console.WriteLine("--------------------------------------------------------------------");
+            List<string> info_sistema = new List<string>();
+            using (StreamReader sr = new StreamReader("info_sistema.txt"))
+            {
+                string riga = sr.ReadLine();
+                while (riga != null)
+                {
+                    info_sistema.Add(riga);      
+                    riga = sr.ReadLine(); 
+                }
+            }            
+            
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("[SESSION START]" + ora);
+                
+                sw.WriteLine("Utente = >" + info_sistema[0]);
+                sw.WriteLine("Sistema = >" + info_sistema[1]);
+                sw.WriteLine("Risoluzione = >" + info_sistema[2]);
+                sw.WriteLine("LINGUA => IT\r\nLIVELLO => ADMIN\r\nAUTOSAVE => ATTIVO\r\n");
+                sw.WriteLine("--------------------");
+                sw.WriteLine("EOF");
+
+
+            }
+
         }
     }
 }
